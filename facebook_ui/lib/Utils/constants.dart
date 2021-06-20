72c67782542d1f0cdb5a2e0d916cfa0e16384acf
Thathsara Senarathne <yasithsenaz2014@gmail.com>
2021-06-20 15:09:27 +0530

    Image assets added to facebook project

--- facebook_ui/lib/Utils/constants.dart
+++ facebook_ui/lib/Utils/constants.dart
@@ -1,8 +1,17 @@
+import 'package:flutter/material.dart';
+
 abstract class Routes {
   static const SPLASH = '/splash';
   static const HOME = '/home';
 }
 
-abstract class AppColors {}
+abstract class AppColors {
+  static const ghost = Color(0xFFC9CCD1);
+  static const dodgerBlue = Color(0xFF2277F2);
+  static const athensGray = Color(0xFFE4E6EB);
+  static const amaranth = Color(0xFFF02849);
+  static const apple = Color(0xFF4BCC33);
+  static const heliotrope = Color(0xFF9D6DF9);
+}
 
 abstract class ImageNames {}
